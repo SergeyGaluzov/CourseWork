7e84c25fc561c62b9610bee80c5639cea4fa6732
Sergey Galuzov <seregasuper2000@gmail.com>
2020-05-30 14:09:13 +0300

    add basic geometric figures

---
+++ src/Figure.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricFigures
+{
+	public abstract class Figure
+	{
+		public int VertexNumber { private set; get; }
+		public List<Vertex> FigureStructure;
+		public Figure(List<Vertex> vertices)
+		{
+			FigureStructure = vertices;
+		}
+		public int GetVertexNumber() => VertexNumber;
+	}
+
+	public class PlaneFigure : Figure
+	{
+		public PlaneFigure(List<Vertex> vertices) : base(vertices) { }
+
+	}
+
+	public class VolumetricFigure : Figure
+	{
+		public VolumetricFigure(List<Vertex> vertices) : base(vertices) { }
+	}
+}
---
+++ src/Vertex.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricFigures
+{
+	public class Vertex
+	{
+		private double x;
+		private double y;
+		private double? z;
+
+		public Vertex(double x, double y, double? z = null)
+		{
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+	}
+}
